5a751e223493370bf5690a1863d001e23b320479
Scott Schroeder <snschroeder89@gmail.com>
2019-08-27 15:35:49 -0500

    responsive styling done, aria-live added, everything looks okay in axe

--- scripts/item.js
+++ scripts/item.js
@@ -1,17 +1,11 @@
 'use strict';
 
 const Item = (function() {
-    const validateTitle = function(title) {
-        if (!title) throw new TypeError('Title must not be blank')
-    }
-
     const flipExpanded = function(bookmark) {
         bookmark.expanded = !bookmark.expanded;
     }
 
-
     return {
-        validateTitle,
         flipExpanded,
     }
 }());
